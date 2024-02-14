d7aa0b53f8f593d9761f1b9d6b599c338962ca8a
zabackary <137591653+zabackary@users.noreply.github.com>
2024-02-14 22:28:49 +0900

    finish installer

--- src/bin/installer.rs
+++ src/bin/installer.rs
@@ -1,39 +1,205 @@
 use std::{
+    io::Write,
     path::Path,
     process::{Command, Stdio},
 };
 
-use anyhow::{ensure, Context};
+use anyhow::{bail, ensure, Context};
 
-use vexmason::{checkversions, installationlocation::get_installation_path};
+use futures::TryStreamExt;
+use octocrab::models::repos::Asset;
+use tokio::{
+    fs::{self, File},
+    io::{self},
+};
+use tokio_util::compat::FuturesAsyncReadCompatExt;
+use vexmason::{
+    checkversions,
+    installationlocation::{
+        get_installation_path, get_user_directory, VEXCOM_NAME, VEXCOM_OLD_NAME,
+    },
+};
 
-fn main() -> anyhow::Result<()> {
-    ensure!(!cfg!(target_os = "macos"), "at this time, MacOS is not supported. if you would like to support it, create a GitHub issue.");
+const GITHUB_RELEASE_OWNER: &str = "zabackary";
+const GITHUB_RELEASE_REPO: &str = "vexmason";
+
+const VEXCOM_TMP_NAME: &str = "vexcom.tmp";
+
+fn pause(msg: &str) {
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+
+    write!(stdout, "{}", msg).unwrap();
+    stdout.flush().unwrap();
+
+    let _ = stdin.read_line(&mut String::new()).unwrap();
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    ensure!(!cfg!(target_os = "macos"), "At this time, MacOS is not supported. if you would like to support it, create a GitHub issue.");
+    ensure!(cfg!(target_os = "windows"), "At this time, Windows is the only supported OS. Please create a GitHub issue if you would like to help support another.");
 
     checkversions::check_versions()?;
+
+    println!("Welcome to the vexmason installation wizard.");
+    println!("Downloading latest release metadata...");
+    let octocrab = octocrab::instance();
+    let repo = octocrab.repos(GITHUB_RELEASE_OWNER, GITHUB_RELEASE_REPO);
+    let release = repo.releases().get_latest().await?;
+    println!("> Downloaded.");
+
     let installation_directory = get_installation_path(std::env::current_exe().ok().as_deref())?;
     let already_installed = installation_directory.try_exists()?;
     if already_installed {
         // try to update
-        println!("already installed, attempting update");
+        println!("Already installed, attempting update");
+        pause(&format!(
+            "> Press ENTER to update installation at {}",
+            installation_directory
+                .to_str()
+                .unwrap_or("unknown directory")
+        ));
         if installation_directory.join("bin").try_exists()? {
-            println!("uninstalling...");
+            println!("> Uninstalling...");
             std::fs::remove_dir_all(installation_directory.join("bin"))?;
         } else {
-            println!("bin directory does not exist, skipping uninstall step");
+            println!("> bin directory does not exist, skipping uninstall step");
         }
+    } else {
+        pause("Press ENTER to start installation...");
+        println!(
+            "> Installing vexmason into {}",
+            installation_directory
+                .to_str()
+                .unwrap_or("unknown directory")
+        );
     }
+    println!("Installing python-compiler...");
     std::fs::create_dir_all(installation_directory.join("bin"))?;
     let compiler_dir = installation_directory.join("lib").join("python-compiler");
     if compiler_dir.try_exists()? {
-        println!("python-compiler seems to be cloned already, pulling the latest changes...");
+        println!("> python-compiler seems to be cloned already, pulling the latest changes...");
         pull_git_lib(&compiler_dir)?;
     } else {
-        println!("cloning python-compiler...");
+        println!("> Cloning python-compiler...");
         install_git_lib(
             &compiler_dir,
             "https://github.com/zabackary/python-compiler.git",
         )?;
+    }
+
+    println!("Installing vexcom hook...");
+    let arch_dir_name = if cfg!(all(
+        target_os = "windows",
+        any(target_arch = "x86", target_arch = "x86_64")
+    )) {
+        "win32"
+    } else if cfg!(all(target_os = "linux", target_arch = "arm")) {
+        "linux-arm32"
+    } else if cfg!(all(target_os = "linux", target_arch = "aarch64")) {
+        "linux-arm64"
+    } else if cfg!(all(
+        target_os = "linux",
+        any(target_arch = "x86", target_arch = "x86_64")
+    )) {
+        "linux-arm64"
+    } else if cfg!(target_os = "macos") {
+        "osx"
+    } else {
+        bail!("unsupported operating system")
+    };
+    let vexcom_dir = get_user_directory(std::env::current_exe().ok().as_deref())?
+        .join(".vscode")
+        .join("extensions")
+        .join("vexrobotics.vexcode-0.5.0")
+        .join("resources")
+        .join("tools")
+        .join("vexcom")
+        .join(arch_dir_name);
+    if vexcom_dir.join(VEXCOM_OLD_NAME).exists() {
+        println!("> vexcom hook already installed, skipping")
+    } else {
+        let mut found_asset: Option<&Asset> = None;
+        for asset in &release.assets {
+            if asset.name == "vexcom.exe" {
+                found_asset = Some(asset);
+            }
+        }
+        if let Some(asset) = found_asset {
+            println!("> making a backup of {VEXCOM_NAME}...");
+            fs::copy(vexcom_dir.join(VEXCOM_NAME), vexcom_dir.join("vexcom.bak")).await?;
+            println!("> found artifact, downloading to {VEXCOM_TMP_NAME}...");
+            let response = reqwest::get(asset.browser_download_url.clone())
+                .await
+                .with_context(|| "failed to fetch artifact")?
+                .error_for_status()
+                .with_context(|| "failed to fetch artifact")?;
+            let body = response.bytes_stream();
+            let mut out = File::create(vexcom_dir.join(VEXCOM_TMP_NAME))
+                .await
+                .with_context(|| "failed to create tmp file to read download")?;
+            io::copy(
+                &mut body
+                    .map_err(|e| futures::io::Error::new(futures::io::ErrorKind::Other, e))
+                    .into_async_read()
+                    .compat(),
+                &mut out,
+            )
+            .await
+            .with_context(|| "failed to copy download content")?;
+            println!("> installing hook...");
+            fs::rename(
+                vexcom_dir.join(VEXCOM_NAME),
+                vexcom_dir.join(VEXCOM_OLD_NAME),
+            )
+            .await?;
+            fs::rename(
+                vexcom_dir.join(VEXCOM_TMP_NAME),
+                vexcom_dir.join(VEXCOM_NAME),
+            )
+            .await?;
+            println!("> successfully installed.");
+        } else {
+            bail!("can't find vexcom.exe artifact in latest release");
+        }
+    }
+    let bin_dir = installation_directory.join("bin");
+    install_bin(&release.assets, "vexmason.exe", &bin_dir).await?;
+    println!("Installation has finished.");
+    pause("Press ENTER to exit...");
+    Ok(())
+}
+
+async fn install_bin(assets: &Vec<Asset>, name: &str, dir: &Path) -> anyhow::Result<()> {
+    let mut found_asset: Option<&Asset> = None;
+    for asset in assets {
+        if asset.name == name {
+            found_asset = Some(asset);
+        }
+    }
+    if let Some(asset) = found_asset {
+        println!("> downloading {name}...");
+        let response = reqwest::get(asset.browser_download_url.clone())
+            .await
+            .with_context(|| "failed to fetch artifact")?
+            .error_for_status()
+            .with_context(|| "failed to fetch artifact")?;
+        let body = response.bytes_stream();
+        let mut out = File::create(dir.join(name))
+            .await
+            .with_context(|| "failed to create file to read download")?;
+        io::copy(
+            &mut body
+                .map_err(|e| futures::io::Error::new(futures::io::ErrorKind::Other, e))
+                .into_async_read()
+                .compat(),
+            &mut out,
+        )
+        .await
+        .with_context(|| "failed to copy download content")?;
+    } else {
+        bail!("can't find {} artifact in latest release", name);
     }
     Ok(())
 }
--- src/installationlocation.rs
+++ src/installationlocation.rs
@@ -12,6 +12,13 @@ pub const VEXCOM_OLD_NAME: &str = "vexcom.old";
 #[cfg(target_os = "linux")]
 pub const VEXCOM_OLD_NAME: &str = "vexcom.old";
 
+#[cfg(target_os = "windows")]
+pub const VEXCOM_NAME: &str = "vexcom.exe";
+#[cfg(target_os = "macos")]
+pub const VEXCOM_NAME: &str = "vexcom";
+#[cfg(target_os = "linux")]
+pub const VEXCOM_NAME: &str = "vexcom";
+
 const INSTALLATION_DIRECTORY: &str = "vexmason";
 
 /// Gets the path of the current installation.
@@ -57,6 +64,45 @@ pub fn get_installation_path(reference_path: Option<&Path>) -> anyhow::Result<Pa
         Ok(local_app_data.join(INSTALLATION_DIRECTORY))
     } else if cfg!(target_os = "linux") {
         Ok(PathBuf::from_str("~/.local/bin")?.join(INSTALLATION_DIRECTORY))
+    } else {
+        bail!("unsupported operating system")
+    }
+}
+
+pub fn get_user_directory(reference_path: Option<&Path>) -> anyhow::Result<PathBuf> {
+    if cfg!(target_os = "windows") {
+        Ok(match std::env::var("USERPROFILE") {
+            Ok(dir) => dir.into(),
+            Err(err) => {
+                if let Some(path) = reference_path {
+                    let path = dunce::canonicalize(path)?;
+                    let components: Vec<Component> = path.components().collect();
+                    let root = components
+                        .iter()
+                        .position(|&component| component == Component::RootDir)
+                        .ok_or(anyhow::anyhow!(
+                            "path is relative so the user directory cannot be determined"
+                        ))?;
+                    ensure!(
+                        components.len() > root + 2,
+                        "path is not long enough to determine user directory"
+                    );
+                    ensure!(
+                        // TODO: ensure this works for non-English Windows
+                        // installations
+                        components[root + 1] == Component::Normal(&OsString::from_str("Users")?),
+                        "path is a system path and does not contain /Users/"
+                    );
+                    path.iter().take(root + 3).collect::<PathBuf>()
+                } else {
+                    Err(err).with_context(|| {
+                        "can't get installation path: couldn't recover from missing USERPROFILE"
+                    })?
+                }
+            }
+        })
+    } else if cfg!(target_os = "linux") {
+        Ok(PathBuf::from_str("~")?)
     } else {
         bail!("unsupported operating system")
     }
